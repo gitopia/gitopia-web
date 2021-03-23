263f4e86622f673974df7c4720ee031bf793d4b9
Snehil Buxy <snehil@gitopia.org>
2021-03-23 15:08:54 +0530

    Org  page basic structure

---
+++ components/header.js
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import { Navbar, Nav, Icon, Dropdown, Tag, InputGroup, AutoComplete } from 'rsuite'
+
+const data = [
+  'gitopia',
+  'git-remote-gitopia',
+  'gitopia-mirror-action'
+]
+
+export default function Header() {
+
+    return (
+        <Navbar appearance="subtle">
+          <Navbar.Header>
+            <a href="/" className="navbar-brand logo"><Image src="/logo.svg" alt="Gitopia logo" width={112} height={56}></Image></a>
+          </Navbar.Header>
+          <Navbar.Body>
+            <Nav>
+              <InputGroup inside style={{width: 350, display: 'inline-flex', margin: 10}}>
+                <AutoComplete data={data} placeholder="Search repositories / wallets" />
+                <InputGroup.Addon>
+                  <Icon icon="search" />
+                </InputGroup.Addon>
+              </InputGroup> 
+              <Nav.Item>Repositories</Nav.Item>  
+              <Nav.Item>Proposals</Nav.Item>
+              <Nav.Item>Issues</Nav.Item>        
+            </Nav>
+            <Nav pullRight>
+              <Nav.Item><Tag color="violet">0 LORE</Tag></Nav.Item>
+              <Dropdown icon={<Icon icon="user" />} title="Wallet Disconnected">
+                <Dropdown.Item>Profile</Dropdown.Item>
+                <Dropdown.Item>Settings</Dropdown.Item>
+              </Dropdown>
+            </Nav>
+          </Navbar.Body>
+        </Navbar>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from 'rsuite/lib/Navbar'
-import Nav from 'rsuite/lib/Nav'
-import Icon from 'rsuite/lib/Icon'
-import Dropdown from 'rsuite/lib/Dropdown'
-import Tag from 'rsuite/lib/Tag'
+import Head from "next/head";
+import { Button, Grid, Row, Col } from "rsuite";
 
 export default function Home() {
   return (
@@ -15,26 +10,20 @@ export default function Home() {
       </Head>
 
       <main>
-        <Navbar appearance="subtle">
-          <Navbar.Header>
-            <a href="#" className="navbar-brand logo"><Image src="/logo.svg" alt="Gitopia logo" width={112} height={56}></Image></a>
-          </Navbar.Header>
-          <Navbar.Body>
-            <Nav>
-              <Nav.Item>Repositories</Nav.Item>     
-              <Nav.Item>Proposals</Nav.Item>
-              <Nav.Item>Issues</Nav.Item>              
-            </Nav>
-            <Nav pullRight>
-              <Nav.Item><Tag color="violet">0 LORE</Tag></Nav.Item>
-              <Dropdown icon={<Icon icon="user" />} title="Wallet Disconnected">
-                <Dropdown.Item>Profile</Dropdown.Item>
-                <Dropdown.Item>Settings</Dropdown.Item>
-              </Dropdown>
-            </Nav>
-          </Navbar.Body>
-        </Navbar>
+        <Grid>
+          <h3>Popular open source projects</h3>
+          <ul>
+            <li>
+              <Button
+                appearance="link"
+                href="/orgs/z_TqsbmVJOKzpuQH4YrYXv_Q0DrkwDwc0UqapRrE0Do"
+              >
+                Gitopia
+              </Button>
+            </li>
+          </ul>
+        </Grid>
       </main>
     </div>
-  )
+  );
 }
