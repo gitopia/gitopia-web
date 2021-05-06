223a66d4fb823cb7ce0170d478d74df086b56c64
Snehil Buxy <snehil@mudrex.com>
2021-05-06 17:07:01 +0530

    Landing page mobile optimised

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
-import "rsuite/dist/styles/rsuite-dark.css";
-import Header from "../components/header";
-
+// import "tailwindcss/tailwind.css";
+// import Header from "../components/header";
+// import { ChakraProvider } from "@chakra-ui/react";
+// import theme from "../theme";
 function MyApp({ Component, pageProps }) {
   return (
+    // <ChakraProvider theme={theme}>
     <>
       {/* <Header /> */}
       <Component {...pageProps} />
     </>
+    // </ChakraProvider>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          50: "#DBFFDB",
+          DEFAULT: "#66ce67",
+          400: "#8BD18B",
+          900: "#3A9F3B",
+        },
+        purple: {
+          50: "#DBFFDB",
+          DEFAULT: "#883BE6",
+          400: "#b78af0",
+          900: "#5a3b82",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [],
+};
