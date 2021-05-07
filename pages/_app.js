ffc7d513cbaa6b8cbc65cc3eb36be9c23dbf4391
Snehil Buxy <snehil@mudrex.com>
2021-05-07 14:35:14 +0530

    Made landning page as index. Combined images and added lazy loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,12 @@
 import "../styles/globals.css";
-// import "tailwindcss/tailwind.css";
 // import Header from "../components/header";
-// import { ChakraProvider } from "@chakra-ui/react";
-// import theme from "../theme";
+
 function MyApp({ Component, pageProps }) {
   return (
-    // <ChakraProvider theme={theme}>
     <>
       {/* <Header /> */}
       <Component {...pageProps} />
     </>
-    // </ChakraProvider>
   );
 }
 
