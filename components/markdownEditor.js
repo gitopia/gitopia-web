9734a5560b85a1add8d4fc2154c8b01b94956bff
Snehil Buxy <snehil@gitopia.org>
2021-07-08 18:32:39 +0530

    Init MD editor

---
+++ components/markdownEditor.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import ReactMde from "react-mde";
+import ReactMarkdown from "react-markdown";
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+function MarkdownEditor(props) {
+  const [value, setValue] = useState("");
+  const [selectedTab, setSelectedTab] = useState("write");
+
+  return (
+    <div className="text-neutral">
+      <ReactMde
+        value={value}
+        onChange={setValue}
+        selectedTab={selectedTab}
+        onTabChange={setSelectedTab}
+        generateMarkdownPreview={(markdown) => {
+          return Promise.resolve(<ReactMarkdown>{markdown}</ReactMarkdown>);
+        }}
+        childProps={{
+          writeButton: {
+            tabIndex: -1,
+          },
+        }}
+      />
+    </div>
+  );
+}
+
+export default MarkdownEditor;
