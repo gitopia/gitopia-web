0fce03c467cb70ccbb5f3561961e803b6829f0d4
Snehil <snehil@gitopia.org>
2021-09-29 11:19:05 +0530

    Updated tabs design

--- components/repository/mainTabs.js
+++ components/repository/mainTabs.js
@@ -11,17 +11,18 @@ export default function RepositoryMainTabs({ hrefBase, active, showSettings }) {
               (active === "code" ? " tab-active" : "")
             }
           >
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 mr-2"
-              stroke="currentColor"
-            >
-              <path d="M9.5 7L4.5 12L9.5 17" strokeWidth="2" />
-              <path d="M14.5 7L19.5 12L14.5 17" strokeWidth="2" />
-            </svg>
-
+            <span className="icon mr-2">
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                stroke="currentColor"
+              >
+                <path d="M9.5 7L4.5 12L9.5 17" strokeWidth="2" />
+                <path d="M14.5 7L19.5 12L14.5 17" strokeWidth="2" />
+              </svg>
+            </span>
             <span>Code</span>
           </a>
         </Link>
@@ -32,19 +33,20 @@ export default function RepositoryMainTabs({ hrefBase, active, showSettings }) {
               (active === "issues" ? " tab-active" : "")
             }
           >
-            <svg
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 mr-2"
-            >
-              <path
-                transform="translate(0,2)"
-                d="M5.93782 16.5L12 6L18.0622 16.5H5.93782Z"
-                stroke="currentColor"
-                strokeWidth="2"
-              />
-            </svg>
-
+            <span className="icon mr-2">
+              <svg
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 mr-2"
+              >
+                <path
+                  transform="translate(0,2)"
+                  d="M5.93782 16.5L12 6L18.0622 16.5H5.93782Z"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                />
+              </svg>
+            </span>
             <span>Issues</span>
           </a>
         </Link>
@@ -87,18 +89,20 @@ export default function RepositoryMainTabs({ hrefBase, active, showSettings }) {
                 (active === "settings" ? " tab-active" : "")
               }
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 mr-2"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <span className="icon mr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 mr-2"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
               <span>Settings</span>
             </a>
           </Link>
