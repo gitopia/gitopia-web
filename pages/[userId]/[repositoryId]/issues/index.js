6533ef847d7de568ffc1b3cd6a986dfb88ea9fa0
Snehil <snehil@gitopia.org>
2021-11-09 10:44:59 +0530

    Updated pagination for issues

--- pages/[userId]/[repositoryId]/issues/index.js
+++ pages/[userId]/[repositoryId]/issues/index.js
@@ -32,9 +32,10 @@ function RepositoryIssueView(props) {
   );
   const [filterText, setFilterText] = useState("is:open");
   const [filters, setFilters] = useState([{ key: "is", value: "open" }]);
-  const [page, setPage] = useState(null);
+  const [page, setPage] = useState(1);
   const [pagination, setPagination] = useState({
-    next_key: null,
+    limit: 10,
+    countTotal: true,
   });
 
   const getAllIssues = async () => {
@@ -80,60 +81,185 @@ function RepositoryIssueView(props) {
         repository.name,
         option,
         {
-          next_key: page,
-          limit: 10,
+          ...pagination,
+          offset: (page - 1) * pagination.limit,
         }
-        // {
-        //   ...pagination,
-        //   offset: (page - 1) * pagination.limit,
-        // }
       );
       console.log(data);
       if (data.Issue) setAllIssues(data.Issue);
-      if (data.pagination) setPagination(data.pagination);
-      // if (pagination) setPage({ ...page, ...pagination });
+      if (data.pagination) setPagination({ ...pagination, ...data.pagination });
     }
   };
 
-  // const renderPagination = () => {
-  //   const { total, offset, limit } = pagination;
-  //   const noOfPages = Math.ceil(total / limit);
-  //   const beforePage =
-  //     page >= 2 ? (
-  //       <button className="btn btn-sm btn-disabled">{page - 1}</button>
-  //     ) : (
-  //       ""
-  //     );
-  //   const firstPage =
-  //     page >= 3 && noOfPages >= 3 ? (
-  //       <button className="btn btn-sm btn-disabled">1</button>
-  //     ) : (
-  //       ""
-  //     );
-  //   const afterPage =
-  //     noOfPages - page >= 2 ? (
-  //       <button className="btn btn-sm btn-disabled">{page + 1}</button>
-  //     ) : (
-  //       ""
-  //     );
-  //   const lastPage =
-  //     noOfPages - page >= 3 && noOfPages >= 3 ? (
-  //       <button className="btn btn-sm btn-disabled">{noOfPages}</button>
-  //     ) : (
-  //       ""
-  //     );
-  //   return (
-  //     <>
-  //       <button className="btn btn-sm btn-disabled">&laquo;</button>
-  //       {firstPage}
-  //       {beforePage}
-  //       <button className="btn btn-sm btn-disabled">{page}</button>
-  //       {afterPage}
-  //       {lastPage}
-  //       <button className="btn btn-sm btn-disabled">&raquo;</button>
-  //     </>
-  //   );
-  // };
+  const renderPagination = () => {
+    const { total, limit } = pagination;
+    const noOfPages = Math.ceil(total / limit);
+    const beforePage =
+      page >= 2 ? (
+        <button
+          className="btn btn-sm"
+          onClick={() => {
+            setPage(page - 1);
+          }}
+        >
+          {page - 1}
+        </button>
+      ) : (
+        ""
+      );
+    const firstPage =
+      page >= 3 && noOfPages >= 3 ? (
+        <button
+          className="btn btn-sm"
+          onClick={() => {
+            setPage(1);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 mt-px"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      ) : (
+        ""
+      );
+    const afterPage =
+      noOfPages - page >= 1 ? (
+        <button
+          className="btn btn-sm"
+          onClick={() => {
+            setPage(page + 1);
+          }}
+        >
+          {page + 1}
+        </button>
+      ) : (
+        ""
+      );
+    const lastPage =
+      noOfPages - page >= 2 && noOfPages >= 3 ? (
+        <button
+          className="btn btn-sm"
+          onClick={() => {
+            setPage(noOfPages);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 mt-px"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+            <path
+              fillRule="evenodd"
+              d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      ) : (
+        ""
+      );
+    const previousPage =
+      page >= 2 ? (
+        <button
+          className="btn btn-sm"
+          onClick={() => {
+            setPage(page - 1);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 mt-px"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      ) : (
+        <button className="btn btn-sm btn-disabled">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 mt-px"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      );
+
+    const nextPage =
+      noOfPages - page >= 1 ? (
+        <button
+          className="btn btn-sm"
+          onClick={() => {
+            setPage(page + 1);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 mt-px"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      ) : (
+        <button className="btn btn-sm btn-disabled">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 mt-px"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      );
+    return (
+      <>
+        {firstPage}
+        {previousPage}
+        {beforePage}
+        <button className="btn btn-sm btn-active">{page}</button>
+        {afterPage}
+        {nextPage}
+        {lastPage}
+      </>
+    );
+  };
 
   useEffect(getAllIssues, [repository, filters, page]);
 
@@ -601,10 +727,10 @@ function RepositoryIssueView(props) {
               })}
             </div>
           </div>
-          {/* <div className="mt-8 flex btn-group justify-center">
+          <div className="mt-8 flex btn-group justify-center">
             {renderPagination()}
-          </div> */}
-          <div className="mt-8 text-center">
+          </div>
+          {/* <div className="mt-8 text-center">
             <button
               className="btn btn-sm btn-wide"
               onClick={() => {
@@ -614,7 +740,7 @@ function RepositoryIssueView(props) {
             >
               Load More
             </button>
-          </div>
+          </div> */}
         </main>
       </div>
       <Footer />
