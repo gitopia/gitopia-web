4bb88bcc89f4d2f507d8b4def3d4158745ffe9a1
Abhiti Darbar <abhiti@gitopia.org>
2022-01-31 15:59:03 +0530

    fetched txs through websocket

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,8 @@ import NotificationManager from "../components/notificationManager";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import ErrorHandler from "../pages/errorHandler";
+import { useState, useEffect } from "react";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
 const progress = new ProgressBar({
   size: 2,
@@ -23,6 +25,36 @@ Router.events.on("routeChangeError", progress.finish);
 dayjs.extend(relativeTime);
 
 function MyApp({ Component, pageProps }) {
+  const ws = new W3CWebSocket("ws://localhost:26657/websocket");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          jsonrpc: "2.0",
+          method: "subscribe",
+          params: ["tm.event='NewBlock'"],
+          id: 1,
+        })
+      );
+    };
+    ws.onmessage = (message) => {
+      let evalData = JSON.parse(message.data);
+      let jsonData = evalData.result.data;
+      if (jsonData !== undefined) {
+        console.log("txs", jsonData.value.block.data.txs);
+        var base64string = String(jsonData.value.block.data.txs[0]);
+        let bufferObj = Buffer.from(base64string, "base64");
+        let decodedString = bufferObj.toString("utf8");
+        console.log("............", decodedString);
+      }
+    };
+
+    ws.onerror = (error) => {
+      console.log(`WebSocket error: ${error}`);
+    };
+  });
   return (
     <>
       <ErrorHandler>
