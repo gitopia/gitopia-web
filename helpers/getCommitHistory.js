94e39a52a7ce8c5227934a2a8e71634d8cb9b769
Snehil <snehil@gitopia.org>
2022-02-08 02:52:52 +0530

    Pagination added to repo index page

--- helpers/getCommitHistory.js
+++ helpers/getCommitHistory.js
@@ -1,8 +1,15 @@
 const validSha = new RegExp(/^[a-f0-9]{40}$/);
 
-export default async function getCommit(repoId = null, commitSha = null) {
+export default async function getCommitHistory(
+  repoId = null,
+  initCommitSha = null,
+  path = null,
+  limit = 100,
+  nextKey = null,
+  shouldGetTotalCount = true
+) {
   let obj = {};
-  if (!validSha.test(commitSha)) {
+  if (!validSha.test(initCommitSha)) {
     return obj;
   }
   let baseUrl =
@@ -11,9 +18,17 @@ export default async function getCommit(repoId = null, commitSha = null) {
       : process.env.NEXT_PUBLIC_OBJECTS_URL + "/commits";
   let params = {
     repository_id: Number(repoId),
+    init_commit_id: initCommitSha,
+    pagination: {
+      count_total: shouldGetTotalCount,
+      limit: limit,
+    },
   };
-  if (commitSha) {
-    baseUrl = baseUrl + "/" + commitSha;
+  if (path) {
+    params.path = path;
+  }
+  if (nextKey) {
+    params.pagination.nextKey = nextKey;
   }
   console.log("params", params);
   await fetch(baseUrl, {
