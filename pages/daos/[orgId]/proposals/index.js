bca860ae99bb166356f63f21b65ca081810a0162
Abhiti Darbar <abhiti@gitopia.org>
2022-02-15 03:09:02 +0530

    added proposal view

--- pages/daos/[orgId]/proposals/index.js
+++ pages/daos/[orgId]/proposals/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Header from "../../../../components/header";
-
+import dayjs from "dayjs";
 import DashboardSelector from "../../../../components/dashboard/dashboardSelector";
 import TopRepositories from "../../../../components/topRepositories";
 import OrgViewTabs from "../../../../components/dashboard/orgViewTabs";
@@ -10,6 +10,7 @@ import { useRouter } from "next/router";
 import getHomeUrl from "../../../../helpers/getHomeUrl";
 import Link from "next/link";
 import getOrganization from "../../../../helpers/getOrganization";
+import getProposals from "../../../../helpers/getProposals";
 
 function GitopiaProposalsView(props) {
   const hrefBase = "/daos/" + props.currentDashboard;
@@ -19,6 +20,15 @@ function GitopiaProposalsView(props) {
     repositories: [],
   });
   const [allRepos, setAllRepos] = useState([""]);
+  const [proposals, setProposals] = useState([""]);
+
+  const letter = org.id ? org.name.slice(0, 1) : "x";
+
+  const avatarLink =
+    process.env.NEXT_PUBLIC_GITOPIA_ADDRESS === org.address
+      ? "/logo-g.svg"
+      : "https://avatar.oxro.io/avatar.svg?length=1&height=100&width=100&fontSize=52&caps=1&name=" +
+        letter;
 
   useEffect(async () => {
     const o = await getOrganization(router.query.orgId);
@@ -43,6 +53,12 @@ function GitopiaProposalsView(props) {
       router.push(getHomeUrl(props.dashboards, props.currentDashboard));
     }
   }, [props.currentDashboard]);
+
+  useEffect(async () => {
+    const p = await getProposals();
+    console.log(p);
+    setProposals(p);
+  });
 
   return (
     <div
@@ -111,7 +127,7 @@ function GitopiaProposalsView(props) {
                 </Link>
               </div>
             </div>
-            <div className="border-b border-grey w-1/2">
+            <div className="border-b-2 border-grey w-1/2">
               <div className="form-control flex-1 mt-5">
                 <div className="relative">
                   <input
@@ -138,8 +154,73 @@ function GitopiaProposalsView(props) {
                 </div>
               </div>
             </div>
-            <div className="text-left px-5 pt-10 font-style: italic text-base">
-              <h2>No Proposals to Show</h2>
+
+            <div className="mt-10 grid grid-rows-auto grid-cols-2 gap-5">
+              {proposals.length > 0 ? (
+                proposals.map((p) => {
+                  return (
+                    <div className="flex flex-col items-center">
+                      <div className="w-full h-72 border-2 border-grey rounded-lg p-3">
+                        <div className="flex">
+                          <div className="mt-2 ml-4 text-sm uppercase font-bold">
+                            {"#" + p.proposal_id}
+                          </div>
+                          <div className="mt-2 ml-auto font-bold text-xs secondary uppercase mr-3">
+                            {dayjs(p.submit_time).fromNow()}
+                          </div>
+                        </div>
+                        <div className="mt-5 ml-4 text-sm uppercase font-medium text-type">
+                          {typeof p.content !== "undefined"
+                            ? p.content.title
+                            : ""}
+                        </div>
+                        <div className="mt-3 ml-4 text-xs text-type-secondary">
+                          {typeof p.content !== "undefined"
+                            ? p.content.description.slice(0, 250)
+                            : ""}
+                        </div>
+                        <div className="flex mr-5 mt-5 secondary font-bold text-xs ml-4">
+                          {"Voting Start "}
+                          <div className="ml-3 secondary text-xs font-normal text-type-secondary">
+                            {" " + p.voting_end_time}
+                          </div>
+                        </div>
+                        <div className="flex mr-5 mt-1 secondary font-bold text-xs ml-4">
+                          {"Voting End "}
+                          <div className="ml-5 secondary text-xs font-normal text-type-secondary">
+                            {" " + p.voting_start_time}
+                          </div>
+                        </div>
+
+                        <div className="mt-8 w-fit border-t-2 border-grey ml-3 mr-3"></div>
+                        <div className="flex mt-2">
+                          <div className="mr-5 secondary uppercase font-bold text-xs ml-3 text-type-secondary">
+                            {"yes " + parseInt(p.final_tally_result.yes)}
+                          </div>
+                          <div className="mr-5 secondary uppercase font-bold text-xs ml-3 text-type-secondary">
+                            {"abstain " +
+                              parseInt(p.final_tally_result.abstain)}
+                          </div>
+                          <div className="mr-5 secondary uppercase font-bold text-xs ml-3 text-type-secondary">
+                            {"no " + parseInt(p.final_tally_result.no)}
+                          </div>
+                          <div className="mr-5 secondary uppercase font-bold text-xs ml-3 text-type-secondary">
+                            {"no with veto " +
+                              parseInt(p.final_tally_result.no_with_veto)}
+                          </div>
+                          <div className="link link-primary modal-button no-underline ml-auto font-bold text-xs uppercase text-green mr-3">
+                            read proposal
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })
+              ) : (
+                <div className="text-left px-5 pt-10 font-style: italic text-base">
+                  <h2>No Proposals to Show</h2>
+                </div>
+              )}
             </div>
           </main>
         </div>
