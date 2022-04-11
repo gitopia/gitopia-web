2459580815bbc3e097cbb31761d7db260794001e
Abhiti Darbar <abhiti@gitopia.org>
2022-04-12 03:14:49 +0530

    using apollo client to fetch Subscribed data

--- helpers/apolloClient.js
+++ helpers/apolloClient.js
@@ -1,7 +1,38 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import WebSocket from "ws";
+
+const httpLink = new HttpLink({
+  uri: "http://gql.gitopia.dev/v1/graphql",
+});
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://gql.gitopia.dev/v1/graphql",
+    webSocketImpl: require("websocket").w3cwebsocket,
+  })
+);
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
-  uri: "https://gql.gitopia.dev/v1/graphql",
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
