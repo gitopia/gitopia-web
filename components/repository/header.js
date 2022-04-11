a812a452083529747348f388dee6c614b52741f3
Snehil <snehil@gitopia.org>
2022-04-11 14:45:01 +0530

    Add forked icon

--- components/repository/header.js
+++ components/repository/header.js
@@ -50,10 +50,32 @@ function RepositoryHeader({ repository, ...props }) {
                 <a className="btn-link">{shrinkAddress(repository.owner.id)}</a>
               </Link>
             </div>
-            <div className="mr-2 text-type-quaternary">/</div>
+            <div className="mr-2 text-type-tertiary">/</div>
             <Link href={"/" + repository.owner.id + "/" + repository.name}>
               <a className="btn-link">{repository.name}</a>
             </Link>
+            {repository.fork ? (
+              <div className="badge badge-outline ml-2 mt-1 text-type-tertiary">
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-3 w-3"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M11.9998 12.5L6.49982 12.5L6.49982 5.5M11.9998 12.5L17.4998 12.5L17.4998 5.5M11.9998 12.5L11.9998 17.5"
+                    strokeWidth="2"
+                    fill="none"
+                  />
+                  <path d="M14.4998 19.5C14.4998 20.8807 13.3805 22 11.9998 22C10.6191 22 9.49982 20.8807 9.49982 19.5C9.49982 18.1193 10.6191 17 11.9998 17C13.3805 17 14.4998 18.1193 14.4998 19.5Z" />
+                  <circle cx="6.49982" cy="5.5" r="2.5" />
+                  <circle cx="17.4998" cy="5.5" r="2.5" />
+                </svg>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
           <div className="flex mt-2">
             <div className="text-type-secondary text-xs font-semibold uppercase flex">
