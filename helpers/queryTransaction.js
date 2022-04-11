901f94a96236a19fa9fb5a73ac90fd86dc58cca9
Abhiti Darbar <abhiti@gitopia.org>
2022-04-12 03:14:49 +0530

    using apollo client to fetch Subscribed data

--- helpers/queryTransaction.js
+++ helpers/queryTransaction.js
@@ -1,5 +1,6 @@
-import { useQuery, gql } from "@apollo/client";
-const QUERY_TRANSACTIONS = gql`
+import { useQuery, useSubscription, gql } from "@apollo/client";
+
+/*const QUERY_TRANSACTIONS = gql`
   query MyQuery {
     transaction(order_by: { height: desc }) {
       hash
@@ -10,6 +11,30 @@ const QUERY_TRANSACTIONS = gql`
 
 function QueryTransaction() {
   const { loading, error, data } = useQuery(QUERY_TRANSACTIONS);
+
+  if (loading) {
+    console.log("fetching....");
+    return null;
+  }
+  if (error) {
+    console.log(error);
+    return null;
+  }
+  console.log(data);
+  return null;
+}
+*/
+const TRANSACTIONS_SUBSCRIPTION = gql`
+  subscription MySubscription {
+    transaction(order_by: { height: desc }) {
+      hash
+      messages
+    }
+  }
+`;
+
+function QueryTransaction() {
+  const { loading, error, data } = useSubscription(TRANSACTIONS_SUBSCRIPTION);
 
   if (loading) {
     console.log("fetching....");
