532259de097c37d0055a235365dc7f6a65d3dfaa
faza <faza@gitopia.org>
2022-04-15 22:45:27 +0530

    Bump git-remote-gitopia version in install.sh

--- public/install.sh
+++ public/install.sh
@@ -15,7 +15,7 @@ function install {
 	USER="gitopia1dlpc7ps63kj5v0kn5v8eq9sn2n8v8r5z9jmwff"
 	PROG="git-remote-gitopia"
 	MOVE="true"
-	VERSION="0.4.0"
+	VERSION="0.4.1"
 	INSECURE="false"
 	OUT_DIR="/usr/local/bin"
 	OBJECTS_URL="https://server.gitopia.com"
