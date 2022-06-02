834af9872b9fbe28678b9fc4cb44ccc028dab09a
Abhiti Darbar <abhiti@gitopia.org>
2022-06-02 16:18:04 +0530

    fixed arrow in create pull request for mobile

--- pages/[userId]/[repositoryId]/compare/[[...branchTuple]].js
+++ pages/[userId]/[repositoryId]/compare/[[...branchTuple]].js
@@ -280,10 +280,10 @@ function RepositoryCompareView(props) {
                 />
               </div>
             </div>
-            <div className="text-type-quaternary">
+            <div className="text-type-quaternary flex justify-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
+                className="h-6 w-6 rotate-90 sm:rotate-0"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
