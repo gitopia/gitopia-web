657c958cae9f5cc7254519024a950cfff1a1ec8e
Abhiti Darbar <abhiti@gitopia.org>
2022-10-06 03:38:41 +0530

    rename organizationList -> organization-list

--- pages/season-of-blockchains/index.js
+++ pages/season-of-blockchains/index.js
@@ -72,7 +72,7 @@ export default function SeasonOfBlockchains() {
               <li className={menuOpen ? "" : "mr-4"}>
                 <a
                   className="px-3 py-4 md:py-2 flex items-center text-sm text-white font-bold border-b-2 border-white border-opacity-0 transition-all hover:border-opacity-70"
-                  href={"/season-of-blockchains/organizationsList"}
+                  href={"/season-of-blockchains/organizations-list"}
                   target="_blank"
                   rel="noreferrer"
                 >
