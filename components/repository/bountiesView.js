360507fd53740a1d42272b9f2d7bf8cac6657140
Abhiti Darbar <abhiti@gitopia.org>
2022-10-19 08:07:27 +0530

    trimmed down bounties view

--- components/repository/bountiesView.js
+++ components/repository/bountiesView.js
@@ -1,100 +1,165 @@
 import { connect } from "react-redux";
 import { useState, useEffect } from "react";
 import getBounty from "../../helpers/getBounty";
-import shrinkAddress from "../../helpers/shrinkAddress";
+import { useRouter } from "next/router";
 import dayjs from "dayjs";
 import { debounce } from "lodash";
+import Link from "next/link";
 
 function IssueBountyView(props) {
+  const router = useRouter();
   const [bounties, setBounties] = useState([]);
-  const [isHovering, setIsHovering] = useState({ id: null });
+  const [coins, setCoins] = useState([]);
+  const [isHovering, setIsHovering] = useState(false);
   useEffect(async () => {
-    const array = [];
+    const bountyArray = [];
+    const coin = {};
+    const coinArray = [];
     for (var i = 0; i < props.bounties.length; i++) {
       const res = await getBounty(props.bounties[i]);
-      array.push(res);
+      bountyArray.push(res);
+      res.amount.map((a) => {
+        coin[a.denom] !== undefined
+          ? (coin[a.denom].amount = coin[a.denom].amount + parseInt(a.amount))
+          : (coin[a.denom] = { amount: parseInt(a.amount), denom: a.denom });
+      });
     }
-
-    setBounties(array);
+    for (const property in coin) {
+      coinArray.push(coin[property]);
+    }
+    setCoins(coinArray);
+    console.log(coins);
+    setBounties(bountyArray.slice(0, 4));
+    console.log(bounties);
   }, [props.bounties.length]);
   return (
     <div
       className="pt-8 mb-4 ml-2"
       onMouseLeave={debounce(() => {
-        setIsHovering({ id: null });
+        setIsHovering(false);
       }, 100)}
     >
       <div className="font-semibold mb-2 text-sm">Bounties</div>
       <div className="">
-        {bounties.map((b, index) => {
+        {coins.map((c, index) => {
           return (
             <div className="mb-2" key={index}>
-              <div className="stroke-type-secondary hover:stroke-teal text-type-secondary hover:text-teal">
+              <div className="text-type-secondary">
                 <div className="flex">
+                  <div className="mr-2">
+                    <svg
+                      width="32"
+                      height="32"
+                      viewBox="0 0 32 32"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle cx="16" cy="16" r="16" fill="#569E34" />
+                    </svg>
+                  </div>
+                  <div
+                    className={"flex uppercase ml-2 mr-4 pb-3 mt-1"}
+                    onMouseOver={debounce(() => {
+                      setIsHovering(true);
+                    }, 500)}
+                  >
+                    {c.denom}
+                  </div>
                   <svg
-                    width="16"
-                    height="14"
-                    viewBox="0 0 16 14"
+                    width="1"
+                    height="15"
+                    viewBox="0 0 1 15"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
-                    className="mt-1"
+                    className="mt-2.5"
                   >
-                    <path
-                      d="M1.93782 12.5L8 2L14.0622 12.5L1.93782 12.5Z"
-                      stroke-width="2"
-                    />
+                    <rect width="1" height="15" fill="#404450" />
                   </svg>
-                  <div>
-                    <div
-                      className={"flex text-xs ml-2 pb-3"}
-                      onMouseOver={debounce(() => {
-                        setIsHovering(b);
-                      }, 500)}
-                    >
-                      {b.id}
-                    </div>
+
+                  <div
+                    className={"flex ml-4 pb-3 mt-1"}
+                    onMouseOver={debounce(() => {
+                      setIsHovering(true);
+                    }, 500)}
+                  >
+                    {c.amount}
                   </div>
                 </div>
-                {isHovering.id == b.id ? (
-                  <div className="flex card bg-grey-500 w-64 h-24 p-3">
-                    <div className="flex">
-                      <div className="avatar flex-none items-center w-1/6">
-                        <div className={"w-7 h-7 rounded-full"}>
-                          <img
-                            src={
-                              "https://avatar.oxro.io/avatar.svg?length=1&height=100&width=100&fontSize=52&caps=1&name=" +
-                              b.creator.slice(-1)
-                            }
-                          />
-                        </div>
-                      </div>
-                      <div className="text-xs text-type mt-1 w-5/6">
-                        {shrinkAddress(b.creator)} created bounty on{" "}
-                        {dayjs.unix(parseInt(b.createdAt)).format("DD/MM/YYYY")}{" "}
-                      </div>
-                    </div>
+                {isHovering ? (
+                  <div className="flex card bg-grey-500 w-72 h-fit p-3">
+                    {bounties.map((b, index) => {
+                      return (
+                        <div
+                          className={
+                            index == 3 ? "" : "border-b" + " border-grey"
+                          }
+                        >
+                          <div className="ml-6 flex mb-2 mt-2">
+                            <div className="stack stroke-type-secondary rotate-90">
+                              <svg
+                                width="32"
+                                height="32"
+                                viewBox="0 0 32 32"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <circle cx="16" cy="16" r="16" fill="#569E34" />
+                              </svg>
+                              <svg
+                                width="32"
+                                height="32"
+                                viewBox="0 0 32 32"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <circle cx="16" cy="16" r="16" fill="#505D7D" />
+                              </svg>
+                              <svg
+                                width="32"
+                                height="32"
+                                viewBox="0 0 32 32"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <circle cx="16" cy="16" r="16" fill="#ED7061" />
+                              </svg>
+                            </div>
 
-                    <div className="flex mt-auto mb-2">
-                      <div className="text-type text-xs font-semibold">
-                        EXPIRY DATE
-                      </div>
-                      <div className="text-type text-xs ml-auto">
-                        {" "}
-                        {dayjs.unix(parseInt(b.createdAt)).fromNow()}
-                      </div>
-                    </div>
+                            <div className="text-type text-xs ml-2 mt-2">
+                              $12.42310
+                            </div>
+
+                            <div className="text-type text-xs ml-auto mr-2 mt-2">
+                              {dayjs
+                                .unix(parseInt(b.createdAt))
+                                .format("DD/MM/YYYY")}
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                    {props.bounties.length > 4 ? (
+                      <Link
+                        href={
+                          "/" +
+                          router.query.userId +
+                          "/" +
+                          router.query.repositoryId +
+                          "/issues/" +
+                          router.query.issueIid +
+                          "/bounties"
+                        }
+                      >
+                        <a className="btn btn-link font-bold mb-2 text-xs text-green text-center">
+                          Go to Bounty Page
+                        </a>
+                      </Link>
+                    ) : (
+                      ""
+                    )}
                   </div>
                 ) : (
-                  <svg
-                    width="22"
-                    height="2"
-                    viewBox="0 0 22 2"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="ml-6 mb-1"
-                  >
-                    <path d="M0 1L22 0.999998" stroke="#3E4051" />
-                  </svg>
+                  ""
                 )}
               </div>
             </div>
