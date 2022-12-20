50ee3f9ee97c8f09e6c708bb6d027f0ba7f0ff83
Snehil <snehil@gitopia.org>
2022-12-20 12:42:27 +0530

    Fix get address from ledger

--- pages/daos/[daoId]/dashboard.js
+++ pages/daos/[daoId]/dashboard.js
@@ -72,7 +72,7 @@ function DaoDashboard(props) {
           <div className="w-64 border-r border-grey flex flex-col">
             <div className="flex-1">
               <DashboardSelector />
-              {allRepository.length == 0 ? (
+              {allRepository?.length == 0 ? (
                 ""
               ) : (
                 <TopRepositories
