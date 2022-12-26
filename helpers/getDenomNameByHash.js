58768b5a7f9b8807df88c34565ecf4977a34b440
Abhiti Darbar <abhiti@gitopia.org>
2022-12-27 05:26:24 +0530

    added helper to get denom name by denom hash for ibc transfer

---
+++ helpers/getDenomNameByHash.js
@@ -0,0 +1,16 @@
+import { Api } from "../store/ibc.applications.transfer.v1/module/rest";
+const api = new Api({ baseUrl: process.env.NEXT_PUBLIC_API_URL });
+export default async function getDenomNameByHash(denom) {
+  if (!denom) return null;
+  try {
+    const denomHash = denom.slice(4, denom.length);
+    const result = await api.queryDenomTrace(denomHash);
+    if (result.ok) {
+      let denom = result.data.denom_trace.base_denom;
+      console.log(denom);
+      return denom;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
