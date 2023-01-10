479eb58ac601769d39bfed577a4dd15a65e365b4
Abhiti Darbar <abhiti@gitopia.org>
2023-01-10 13:05:44 +0530

    remove console log

--- helpers/getIssueCommentAll.js
+++ helpers/getIssueCommentAll.js
@@ -4,7 +4,6 @@ export default async function getIssueCommentAll(repoId, issueIid) {
   if (!repoId || !issueIid) return null;
   try {
     const res = await api.queryIssueCommentAll(repoId, issueIid);
-    console.log(res);
     if (res.ok) {
       let c = res.data.Comment;
       return c;
