ffce1296b4b1c17484eabe43e3b8eb2f26643f75
Abhiti Darbar <abhiti@gitopia.org>
2023-01-24 07:21:46 +0530

    updating variable name

--- helpers/getChainInfo.js
+++ helpers/getChainInfo.js
@@ -1,7 +1,7 @@
 import { assets } from "../ibc-assets-config";
-export default async function getChainInfo(chainId) {
+export default async function getChainInfo(chainName) {
   let info = assets.filter((asset) => {
-    return asset.chain_name === chainId;
+    return asset.chain_name === chainName;
   });
   return info[0];
 }
