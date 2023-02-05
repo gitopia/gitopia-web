5dc289c7183671c3d9f4f490c88d27ede7f0d117
Abhiti Darbar <abhiti@gitopia.org>
2023-02-06 04:11:00 +0530

    delete tag tx

--- store/actions/repository.js
+++ store/actions/repository.js
@@ -1054,6 +1054,42 @@ export const createTag = ({
   };
 };
 
+export const deleteTag = ({
+  repoOwnerId = null,
+  repositoryName = null,
+  name = null,
+}) => {
+  return async (dispatch, getState) => {
+    if (!(await validatePostingEligibility(dispatch, getState, "tag")))
+      return null;
+
+    const { wallet, env } = getState();
+    const tag = {
+      creator: wallet.selectedAddress,
+      repositoryId: {
+        id: repoOwnerId,
+        name: repositoryName,
+      },
+      tag: name,
+    };
+
+    try {
+      const message = await env.txClient.msgDeleteTag(tag);
+      const result = await sendTransaction({ message })(dispatch, getState);
+      if (result && result.code === 0) {
+        return result;
+      } else {
+        dispatch(notify(result.rawLog, "error"));
+        console.log(result);
+        return null;
+      }
+    } catch (e) {
+      console.error(e);
+      dispatch(notify(e.message, "error"));
+    }
+  };
+};
+
 export const updatePullRequestAssignees = ({
   repositoryId = null,
   pullIid = null,
