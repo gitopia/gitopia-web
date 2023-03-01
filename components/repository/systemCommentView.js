a034eb93f900ab965d850bf8778c162a876bae03
Abhiti Darbar <abhiti@gitopia.org>
2023-03-01 19:14:53 +0530

    system comment ui fixes

--- components/repository/systemCommentView.js
+++ components/repository/systemCommentView.js
@@ -6,16 +6,16 @@ function SystemCommentView({ comment = { creator: "" }, ...props }) {
     <div className="flex w-full" key={"comment" + comment.id}>
       <div className="flex-none mr-4 w-10"></div>
       <div className="flex-none w-12 relative pt-5 flex items-center justify-center">
-        {commentType[comment.commentType] === "" ? (
-          <div className="border border-grey rounded-full w-6 h-6 bg-base-100 z-10 relative top-px"></div>
-        ) : (
+        {commentType[comment.commentType] ? (
           <div className="w-6 h-6 z-10 relative top-px">
             {commentType[comment.commentType]}
           </div>
+        ) : (
+          <div className="border border-grey rounded-full w-6 h-6 bg-base-100 z-10 relative top-px"></div>
         )}
         <div className="border-l border-grey h-1/2 absolute left-1/2 top-0 z-0"></div>
       </div>
-      <div className="flex-1 text-xs text-type-secondary pr-4 pt-6 break-words w-5 sm:w-full">
+      <div className="flex-1 text-xs text-type-secondary pr-4 pt-6 break-words w-5 sm:w-96">
         <ReactMarkdown linkTarget="_blank">{comment.body}</ReactMarkdown>
       </div>
     </div>
