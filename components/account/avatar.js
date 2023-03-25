262a7647df2517757d593609e5ebe324e7835223
Snehil <snehil@gitopia.org>
2023-03-25 13:43:42 +0530

    Verified icon added

--- components/account/avatar.js
+++ components/account/avatar.js
@@ -122,22 +122,25 @@ function AccountAvatar({ isEditable = false, isDao = false, ...props }) {
                   (isDao ? " rounded-md" : " rounded-full")
                 }
               >
-                {previewAvatarUrl ?
-                <img
-                  src={previewAvatarUrl}
-                  onLoad={(e) => {
-                    if (e.target.width > 0) {
+                {previewAvatarUrl ? (
+                  <img
+                    src={previewAvatarUrl}
+                    onLoad={(e) => {
+                      if (e.target.width > 0) {
+                        setPreviewLoading(false);
+                        setValidateImageUrlError(null);
+                        setPreviewAvatarText("");
+                      }
+                    }}
+                    onError={() => {
                       setPreviewLoading(false);
-                      setValidateImageUrlError(null);
+                      setValidateImageUrlError("Unable to preview");
                       setPreviewAvatarText("");
-                    }
-                  }}
-                  onError={() => {
-                    setPreviewLoading(false);
-                    setValidateImageUrlError("Unable to preview");
-                    setPreviewAvatarText("");
-                  }}
-                /> : ""}
+                    }}
+                  />
+                ) : (
+                  ""
+                )}
                 <div className="absolute w-full bottom-16 text-center italic text-grey-300 text-sm">
                   {previewAvatarText}
                 </div>
@@ -338,7 +341,7 @@ function AccountAvatar({ isEditable = false, isDao = false, ...props }) {
                   strokeLinejoin="round"
                   d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
                 />
-              </svg>
+</svg>
             </div>
           </label>
         ) : (
