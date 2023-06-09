c65e24149442193b7bc1094b772116b576bb8b04
Abhiti Darbar <abhiti@gitopia.org>
2023-06-09 13:02:18 +0530

    updating variable value

--- helpers/getChainIbcAssets.js
+++ helpers/getChainIbcAssets.js
@@ -1,7 +1,7 @@
 import axios from "./axiosFetch";
 export default async function getChainIbcAsset(chainName) {
   let chains = [
-    process.env.NEXT_PUBLIC_NETWORK_ENVIRONMENT === "MAINNET"
+    process.env.NEXT_PUBLIC_NETWORK_ENVIRONMENT === "mainnet"
       ? "gitopia"
       : "gitopiatestnet",
     chainName,
