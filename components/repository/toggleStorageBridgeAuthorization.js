d4656f2adbe9f2fb6c3fdc407b3572175224bc6b
faza <faza@gitopia.org>
2024-07-03 18:11:40 +0530

    fix settings page

--- components/repository/toggleStorageBridgeAuthorization.js
+++ components/repository/toggleStorageBridgeAuthorization.js
@@ -2,14 +2,24 @@ import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { updateAddressGrant } from "../../store/actions/user";
 import getStorageBridgeAuthStatus from "../../helpers/getStorageBridgeAuthStatus";
+import { useApiClient } from "../../context/ApiClientContext";
 
 function ToggleStorageBridgeAuthorization({ address, onSuccess, ...props }) {
   const [currentState, setCurrentState] = useState(false);
   const [isToggling, setIsToggling] = useState(true);
+  const { apiClient, cosmosBankApiClient, cosmosFeegrantApiClient } =
+    useApiClient();
 
   const toggleGrant = async () => {
     setIsToggling(true);
-    const res = await props.updateAddressGrant(address, 1, !currentState);
+    const res = await props.updateAddressGrant(
+      apiClient,
+      cosmosBankApiClient,
+      cosmosFeegrantApiClient,
+      address,
+      1,
+      !currentState
+    );
     if (res && res.code === 0) {
       if (onSuccess) await onSuccess(!currentState);
       setCurrentState(!currentState);
@@ -20,7 +30,7 @@ function ToggleStorageBridgeAuthorization({ address, onSuccess, ...props }) {
   useEffect(() => {
     async function initAddress() {
       setIsToggling(true);
-      setCurrentState(await getStorageBridgeAuthStatus(address));
+      setCurrentState(await getStorageBridgeAuthStatus(apiClient, address));
       setIsToggling(false);
     }
     initAddress();
--- helpers/getStorageBridgeAuthStatus.js
+++ helpers/getStorageBridgeAuthStatus.js
@@ -1,9 +1,11 @@
 import { useApiClient } from "../context/ApiClientContext";
 
-export default async function getStorageBridgeAuthStatus(userAddress) {
+export default async function getStorageBridgeAuthStatus(
+  apiClient,
+  userAddress
+) {
   if (!userAddress) return null;
   try {
-    const { apiClient } = useApiClient();
     const res = await apiClient.queryCheckStorageProviderAuthorization(
       userAddress,
       process.env.NEXT_PUBLIC_STORAGE_BRIDGE_WALLET_ADDRESS
