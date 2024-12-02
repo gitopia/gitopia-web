3b7b35497cf16057bf7fcb75526d0190494169c6
faza <faza@gitopia.org>
2024-12-02 21:45:08 +0700

    fix typo in import

--- components/dashboard/dao.js
+++ components/dashboard/dao.js
@@ -20,7 +20,7 @@ import { useRouter } from "next/router";
 import { getBalance } from "../../store/actions/wallet";
 import getAnyRepositoryAll from "../../helpers/getAnyRepositoryAll";
 import sortBy from "lodash/sortBy";
-import DaoRepositories from "./DAORepositories";
+import DaoRepositories from "./DaoRepositories";
 
 function DaoDashboard({ dao = {}, advanceUser, ...props }) {
   const router = useRouter();
